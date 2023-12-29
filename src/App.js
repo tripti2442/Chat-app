d3546d6bd5a617aff235994829f63a95caef46d3
HimanshiMadan <himanshi2957@gmail.com>
2023-12-29 17:48:01 +0530

    Logout-Button Added

--- src/App.js
+++ src/App.js
@@ -9,15 +9,24 @@ const projectID = 'd8916fd5-0420-428e-9599-8229b8ba73a6';
 const App = () => {
   if (!localStorage.getItem('username')) return <LoginForm />;
 
+  const handleLogout = () => {
+    localStorage.removeItem('username');
+    localStorage.removeItem('password');
+    window.location.reload();
+  };
+
   return (
-    <ChatEngine
-      height="100vh"
-      projectID={projectID}
-      userName={localStorage.getItem('username')}
-      userSecret={localStorage.getItem('password')}
-      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-     // onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
-    />
+    <>
+      <button onClick={handleLogout}>Logout</button>
+      <ChatEngine
+        height="100vh"
+        projectID={projectID}
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+       // onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+      />
+    </>
   );
 };
 
